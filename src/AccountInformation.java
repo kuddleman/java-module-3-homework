ef330047b809816f5f806c645d8490b411db979f
Kuddleman <edl1155@gmail.com>
2021-02-22 15:19:53 -0800

    complete coding of AccountInformationBuilder

--- src/AccountInformation.java
+++ src/AccountInformation.java
@@ -5,6 +5,14 @@ public class AccountInformation {
     private String localBranchAddress;
     private String mainBranchAddress;
 
+    private AccountInformation(AccountInformationBuilder builder){
+        this.bankRoutingNumber = builder.bankRoutingNumber;
+        this.bankName = builder.bankName;
+        this.swiftBankCode = builder.swiftBankCode;
+        this.localBranchAddress = builder.localBranchAddress;
+        this.mainBranchAddress = builder.mainBranchAddress;
+    }
+
     public static class AccountInformationBuilder {
         private int bankRoutingNumber; //ok
         private String bankName = "Bank of Seven Seas";
@@ -17,7 +25,25 @@ public class AccountInformation {
         public AccountInformationBuilder(int bankRoutingNumber, int swiftBankCode){
             this.bankRoutingNumber = bankRoutingNumber;
             this.swiftBankCode = swiftBankCode;
+        }
 
+        public AccountInformationBuilder bankName(String var) {
+            this.bankName = var;
+            return this;
+        }
+
+        public AccountInformationBuilder localBranchAddress(String var) {
+            this.localBranchAddress = var;
+            return this;
+        }
+
+        public AccountInformationBuilder mainBranchAddress(String var) {
+            this.mainBranchAddress = var;
+            return this;
+        }
+
+        public AccountInformation build(){
+           return new AccountInformation(this);
         }
 
 
