6094647592c7cdded92abb19ed533ec449216105
Kuddleman <edl1155@gmail.com>
2021-02-22 20:36:23 -0800

    add GeneralLedgerAccounts class for factory method

---
+++ src/GeneralLedgerAccounts.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneralLedgerAccounts {
+    private List<Account> generalLedgerAccountList;
+
+    public GeneralLedgerAccounts() {
+        generalLedgerAccountList = new ArrayList<>();
+    }
+
+
+
+}
